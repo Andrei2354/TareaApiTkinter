cad1483b9ea246053fabd94e6b1acd6b1d27927c
Andrei2354 <andreiaimar1362@gmail.com>
2024-10-16 13:54:50 +0100

    Final de la hota

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
-
+import json
 import requests
+
+import ProductApi
+from ProductApi import Products
 
 URL = "https://dummyjson.com/products"
 response = requests.get(URL)
-print('Data:', response.json())
+data1 = response.json()
+data_String = json.dumps(data1)
+# print('Data:', response.json())
 
+data_String1 = '{"id": 1}'
+
+my_obj = json.loads(data_String1, object_hook=lambda data: Products(**data))
+print(data_String)
+print(my_obj)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://dummyjson.com/products Dummyjson 
 https://4geeks.com/es/how-to/como-consumir-una-api-en-python Api
+buscar una manera con object_hook sacarlo de la api
+https://dev.to/mandrewcito/json-to-python-object-5cl9 
