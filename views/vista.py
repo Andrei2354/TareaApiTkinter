45a66708f6e93b7b78b6e049460453858047eb0e
Andrei2354 <andreiaimar1362@gmail.com>
2024-10-18 09:49:03 +0100

    Se muestra la imagen

--- views/vista.py
+++ views/vista.py
@@ -4,9 +4,23 @@ from shutil import which
 from tkinter import ttk
 from tkinter import *
 import random
+from PIL import ImageTk, Image
+import requests
+
 from models.Main import product_list
+label_imagen_producto = ""
+def carrusel():
+    global indice, contenido, label_imagen_producto
+    if indice < len(product_list.products):
+         bits_imagen = requests.get(product_list.products[indice].thumbnail, stream=True)
+         imagen = Image.open(bits_imagen.raw)
+         imagen_tk = ImageTk.PhotoImage(imagen)
+         label_imagen_producto.config(image=imagen_tk)
+         label_imagen_producto.image = imagen_tk
+
 
 def modelo_principal():
+    global indice, contenido, label_imagen_producto
     root = tk.Tk()  # Ventana principal
     canvas = tk.Canvas(root)
     root.geometry("800x800")
@@ -19,8 +33,8 @@ def modelo_principal():
     titulo = ttk.Frame(root, style="1.TLabel",  width=800, height=100)
     titulo.pack(side="top", fill=X)
 
-    label1 = Label(titulo, text="Productos").pack(ipady="12", ipadx="20")
-    label1.config(bg="green")
+    label1 = Label(titulo, text="Productos", background="#fc9403")
+    label1.pack(ipady="12", ipadx="20")
 
     buscador = ttk.Frame(root, style="2.TLabel", width=800, height=50)
     buscador.pack(side="top", fill=X)
@@ -29,12 +43,18 @@ def modelo_principal():
     contenido.pack(side="top", expand=True, fill=BOTH)
 
     resultado = tk.StringVar()
-    for product in product_list.products:
-        resultado.set(product.title)
+    indice = 1
 
 
     boton_resultado = ttk.Button(contenido, textvariable=resultado)
     boton_resultado.pack(side="bottom", fill=NONE)
+
+    label_imagen_producto = ttk.Label(contenido,background="#dff5f0", text="Jugar")
+    label_imagen_producto.pack(side="bottom", ipady="2", ipadx="20")
+
+    carrusel()
     canvas.mainloop()
+
+
 
 modelo_principal()
